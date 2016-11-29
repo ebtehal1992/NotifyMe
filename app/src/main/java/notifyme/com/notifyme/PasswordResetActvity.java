afeb2df0c3fe7a9196d61de517828f6d40f2c5fc
ebtehal1992 <ea.almathami@gmail.com>
2016-11-29 18:04:54 +0000

    structure ready

---
+++ app/src/main/java/notifyme/com/notifyme/PasswordResetActvity.java
@@ -0,0 +1,13 @@
+package notifyme.com.notifyme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PasswordResetActvity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_reset_actvity);
+    }
+}
