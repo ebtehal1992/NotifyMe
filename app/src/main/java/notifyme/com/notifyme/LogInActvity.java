cab87b812199fe745cef6bba9965cfe2fa5ff3f9
ebtehal1992 <ea.almathami@gmail.com>
2016-12-01 18:04:19 +0000

    Authentication completed

--- app/src/main/java/notifyme/com/notifyme/LogInActvity.java
+++ app/src/main/java/notifyme/com/notifyme/LogInActvity.java
@@ -1,13 +1,128 @@
 package notifyme.com.notifyme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
 
 public class LogInActvity extends Activity {
+    //Declare UI Components as variable
+    public Button mLoginButton;
+    public Button mForgetButton;
+    public Button mRegisterButton;
+    public EditText mUsernameEditText;
+    public EditText mPasswordEditText;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_actvity);
+        // Initialize UI components
+        mLoginButton = (Button)findViewById(R.id.loginLoginButton);
+        mForgetButton = (Button)findViewById(R.id.loginForgetButton);
+        mRegisterButton = (Button)findViewById(R.id.loginRegisterButton);
+        mUsernameEditText = (EditText)findViewById(R.id.loginUsernameEditText);
+        mPasswordEditText = (EditText)findViewById(R.id.loginPasswordEditText);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+
+                    // User is signed in
+
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+
+                } else {
+
+                    // User is signed out
+
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+
+                }
+
+            }
+
+        };
+
+        //Set onclick button for login
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String mEmail = mUsernameEditText.getText().toString();
+                final String mPassword = mPasswordEditText.getText().toString();
+
+                // [START sign_in_with_email]
+
+                mAuth.signInWithEmailAndPassword(mEmail, mPassword)
+
+                        .addOnCompleteListener(LogInActvity.this, new OnCompleteListener<AuthResult>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                                if (task.isSuccessful()) {
+                                    Intent takeUserToLogin = new Intent(LogInActvity.this, MainActivity.class);
+                                    startActivity(takeUserToLogin);
+                                    Toast.makeText(LogInActvity.this, "Successfully Login", Toast.LENGTH_SHORT).show();
+
+                                }else{
+
+                                    Toast.makeText(LogInActvity.this, "Please check your login credential", Toast.LENGTH_SHORT).show();
+
+                                }
+
+                            }
+
+                        });
+
+            }
+        });
+        //Set onclick button for Forget password
+        mForgetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent takeUserForgetPassword = new Intent(LogInActvity.this, PasswordResetActvity.class);
+                startActivity(takeUserForgetPassword);
+
+            }
+        });
+        //Set onclick button for Register
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent takeUserToRegister = new Intent(LogInActvity.this, RegisterActivity.class);
+                startActivity(takeUserToRegister);
+            }
+        });
+
     }
 }
